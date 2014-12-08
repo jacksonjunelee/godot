759c6ee70d76795459e202cfecbf3849158a73f2
Loaner 2 <vanderhoop@me.com>
2014-12-08 08:31:27 -0500

    adds bot updates

--- w01/d05/student/eliza_practice.rb
+++ w01/d05/student/eliza_practice.rb
@@ -22,20 +22,20 @@ def get_user_input
    $stdin.gets.chomp.downcase
 end
 
-responses_declarative = ["Oh wow, interesting.", "That's quite intriguing.", "You're a smart one."]
-responses_inquiring = ["Never!", "Once upon a time...", "Catch me if you can."]
-responses_exclamation = ["Dude!", "Duuuude.", "Oh mi gosh!"]
+# responses_declarative = ["Oh wow, interesting.", "That's quite intriguing.", "You're a smart one."]
+# responses_inquiring = ["Never!", "Once upon a time...", "Catch me if you can."]
+# responses_exclamation = ["Dude!", "Duuuude.", "Oh mi gosh!"]
 
 loop do
   alert("Hi there, I'm Eliza! Let's have a chat.")
   user_input = get_user_input
   $stdout.puts(user_input + "!")
 
-    break
-    if user_input == "goodbye"
-    else
-
+  loop do
     $stdout.puts("hmmm")
+    break if
+      user_input == "goodbye"
+  end
 
 
   # When the user inputs the string:
---
+++ w01/d05/student/shylockbot.rb
@@ -0,0 +1,42 @@
+require 'pry'
+require 'colorize'
+
+
+# $stdin.gets.chomp
+
+def use_shylock_money
+  (10000).to_i
+end
+
+def accounts(ducats)
+  $stdin.gets(prompt)
+  $stdin.gets.chomp
+end
+
+loans = {}
+
+loop do
+  owed = use_shylock_money
+  $stdout.print(">> ")
+  loop do
+    if $stdin.gets("purse")
+      $stdout.puts(":: ShylockBot pulls out his purse ::")
+      $stdout.puts("10000 ducats")
+      $stdout.print(">> ")
+    else
+      $stdin.gets("loan Antonio #{amount} ducats")
+      loans[ducats] = []
+      loans[ducats].push(amount)
+      $stdout.puts("::ShylockBot gives Antonio #{amount} ducats::")
+      $stdout.puts("-#{amount}")
+  # else
+    # balance = loans("10000 - #{loans} ducats").to_i
+# $stdin.gets.to_i
+    end
+end
+
+end
+# # purse = {}
+# borrowers = ["Antonio","Bassanio"]
+# end
+# binding.pry
