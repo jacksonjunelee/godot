bbda145599b21d5423db2737151e61de3aa40d28
Loaner 2 <vanderhoop@me.com>
2014-12-09 09:01:24 -0500

    adds homework updates

--- w02/d01/student/bond_answers.rb
+++ w02/d01/student/bond_answers.rb
@@ -9,9 +9,6 @@ BOND_DATA.each {|movie_gross| total.push( movie_gross[:gross] ) }
 movie_gross
 
 
-
-
-
 movie_gross = 0
 BOND_DATA.each do |total|
 
@@ -46,8 +43,10 @@ BOND_DATA.each {|film_titles| films.push( film_titles[:title])}
 puts films
 
 # 4. Create a new array of hashes of only the bond films released on odd-numbered years. Assign it to a sensibly-named variable.
-
-odd_year = ([:year] % 2 != 0)
+# --------------HELP!!!------------------
+odd_movie_years = []
+odd_year = ([:year] / 2 != 0)
+BOND_DATA.each {|odd_year| odd_movie_years.push(odd_year([:year] % 2 != 0))}
 if odd_year
 
 # 5. Create a variable called worst_grossing_bond. Use all the tools at your disposal (and perhaps one or two you have yet to discover) to iterate through all the films and return the film with the lowest gross. Store the returned film hash in the variable worst_grossing_bond.
@@ -65,3 +64,6 @@ BOND_DATA.each {|movie_gross| highest_grossing_bond.push( movie_gross[:gross])}
 puts highest_grossing_bond.first
 
 # 7. Create a new array, movies_per_actor, where each value in the array is a hash where the key is the Bond actor's name and the value is their total number of films. For example { "Daniel Craig" => 3 }
+
+movies_per_actor = []
+movies_per_actor.push({:actor => 3})
