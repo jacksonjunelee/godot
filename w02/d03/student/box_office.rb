2768d9a17062dbfdf8d51441229411aac4731ba8
Loaner 2 <vanderhoop@me.com>
2014-12-10 09:16:33 -0500

    adds homework comments

---
+++ w02/d03/student/box_office.rb
@@ -0,0 +1,13 @@
+# Transform the following string of ratings into an array of strings.
+
+rating_string = "pg-thirteen,pg,r,pg,pg-thirteen,pg-thirteen,pg-thirteen,r,r,r,r,pg-thirteen,r,pg-thirteen,r,r,r,r,r,r,pg-thirteen,r,pg-thirteen,pg-thirteen,pg,r,r,pg,pg".split(",")
+ratings = []
+ratings.push(rating_string)
+puts ratings
+
+movie_titles_string = "the hunger games: mockingjay - part 1|penguins of madagascar|horrible bosses 2|big hero 6|interstellar|dumb and dumber to|the theory of everything|gone girl|the pyramid|birdman|nightcrawler|st. vincent|fury|beyond the lights|wild|foxcatcher|the homesman|whiplash|john wick|the equalizer|guardians of the galaxy|the judge|the maze runner|ouija|alexander and the terrible, horrible, no good, very bad day|rosewater|hector and the search for happiness|the book of life|the boxtrolls".split("|")
+movie_titles = []
+movie_titles.push(movie_titles_string)
+puts movie_titles
+
+
