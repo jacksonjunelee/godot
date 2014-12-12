46381ee1fb280b0d1f17c33501fa97cc00b1bfef
Loaner 2 <vanderhoop@me.com>
2014-12-12 13:44:29 -0500

    adds morning exercises

--- w02/d05/student/hamlet.rb
+++ w02/d05/student/hamlet.rb
@@ -1,3 +1,4 @@
+require 'pry'
 hamlets = [
   "Laurence Olivier, 1948",
   "Grigori Kozintsev, 1964",
@@ -6,6 +7,40 @@ hamlets = [
   "Kenneth Branagh, 1996",
   "Michael Almereyda, 2000"
 ]
+
+hamlets.each do |ham_line|
+  ham_line #is the string
+  actor_and_year = ham_line.split(",")
+  actor = actor_and_year[0]
+  year = actor_and_year[1]
+  msg = "O! the venerable #{actor} starred as Hamlet in #{year}"
+  puts(msg)
+end
+
+hamlets_array = hamlets.map do |ham_line| ham_line
+  name_and_year = ham_line.split(",")
+  year = name_and_year[1].to_i
+  name = name_and_year[0]
+  [name, year]
+end
+puts hamlets_array
+
+hammertime = hamlets_array.map do |ham_array|
+  name = ham_array[0]
+  year = ham_array[1]
+  {:name => name,
+  :year => year}
+end
+
+swinging_hamlets = hammertime.select { |hammertime| hammertime[:year].between?(1959, 1970)}
+# ----------------or-----------
+year = hammertime[:year]
+year.to_s.include?("196")
+
+future_hamlets = hammertime.select { |hammertime| hammertime[:year].between?(1999, 2015)}
+# ----------or--------------
+hammertime[:year].between?(1999, 2015)
+
 
 quotes_array = [
   ["To be, or not to be: that is the question", "Hamlet - (Act III, Scene I)."],
@@ -28,3 +63,31 @@ quotes_array = [
   ["What a piece of work is man! how noble in reason! how infinite in faculty! in form and moving how express and admirable! in action how like an angel! in apprehension how like a god! the beauty of the world, the paragon of animals! ", "Hamlet - (Act II, Scene II)."],
   ["When sorrows come, they come not single spies, but in battalions", "Hamlet - (Act IV, Scene V)."]
 ]
+
+quotes_array.each do |sample_quote|
+  sample_quote #is the string
+  quote_play_act_scene = sample_quote.split(",").split(":").split("-")
+  quote = quote_play_act_scene[0]
+  play = quote_play_act_scene[1]
+  act = quote_play_act_scene[2]
+  scene = quote_play_act_scene[3]
+end
+
+hamlets_array = hamlets.map do |ham_line| ham_line
+  name_and_year = ham_line.split(",")
+  year = name_and_year[1].to_i
+  name = name_and_year[0]
+  [name, year]
+end
+puts hamlets_array
+
+hammertime = hamlets_array.map do |ham_array|
+  name = ham_array[0]
+  year = ham_array[1]
+  {:name => name,
+  :year => year}
+end
+
+
+
+binding.pry
