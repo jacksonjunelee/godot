3c51d122c5fe331bb97201a5a88d6bb204248b8a
Kamari Aykes <aykeskc@gmail.com>
2015-01-12 16:48:50 -0500

    adds sinatra 0auth redirects

--- w05/d01/student/sinatra.rb
+++ w05/d01/student/sinatra.rb
@@ -3,21 +3,27 @@ require_relative 'diary'
 
 module SinatraApp
   class Server < Sinatra::Base
+      enable :sessions
 
   configure :development do
       register Sinatra::Reloader
       $persons_secrets = Redis.new
   end
 
+
     get '/' do
       render :erb, :index, layout: :default
     end
 
     get '/login' do
+      session[:name]      = params[:name]
+      session[:timestamp] = Time.now
       redirect to(params[:name])
     end
 
     get '/:username' do
+      if session[:name] != params[:username]
+        redirect to('/')
       @name = params[:username]
       @secrets = $persons_secrets.lrange "diary_app:#{@name}", 0, -1
       render :erb, :user, layout: :default
