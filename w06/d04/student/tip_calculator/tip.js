4a5cc75c0f98825cdd0689a22addf672b8784229
Kamari Aykes <aykeskc@gmail.com>
2015-01-22 14:51:10 -0500

    adds tip calculator solution

---
+++ w06/d04/student/tip_calculator/tip.js
@@ -0,0 +1,19 @@
+console.log("loaded tip.js");
+
+var amountControl = document.querySelector("#amount");
+var taxControl = document.querySelector("#tax");
+var calculateTotalBill = function(){
+  // get the amt from the input
+  // convert the amount to float
+  var amount = parseFloat(amountControl.value);
+  // get the tax
+  // convert the amt to a float
+  var tax = parseFloat(taxControl.value);
+  // assign 0.2 to tip
+  var tip = 0.2;
+  // assign tipAmount to amount * tip
+  var tipAmount = (amountControl.value * tip);
+  // return tipAmount + (amount * tax) + amount
+  var total = (tipAmount + (amount * tax) + amount);
+  return total
+};
