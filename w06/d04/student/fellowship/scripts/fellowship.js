e7d993eef9a3e356eddee2b93a7495238cb9786a
Kamari Aykes <aykeskc@gmail.com>
2015-01-23 08:39:55 -0500

    adds fellowship.js hw help request

--- w06/d04/student/fellowship/scripts/fellowship.js
+++ w06/d04/student/fellowship/scripts/fellowship.js
@@ -1,3 +1,5 @@
+// I AM SOOOOOOOOO LOST! PLEASE HELP!!!!!!//
+
 console.log("Linked.");
 
 // Dramatis Personae
@@ -19,4 +21,35 @@ var buddies = [
 var lands = ['The Shire', 'Rivendell', 'Mordor'];
 var body = document.querySelector('body');
 
+function makeMiddleEarth() {
+  // create a section tag with an id of middle-earth
+  var sectionTag = document.createElement("section");
+  // add each land as an article tag
+  // inside each article tag include an h1 with the name of the land
+  // append middle-earth to your document body
 
+};
+
+makeMiddleEarth();
+
+function makeHobbits() {
+  // display an unordered list of hobbits in the shire (which is the second article tag on the page)
+  // give each hobbit a class of hobbit
+}
+
+function keepItSecretKeepItSafe() {
+  // create a div with an id of 'the-ring'
+  // give the div a class of 'magic-imbued-jewelry'
+  // add an event listener so that when a user clicks on the ring, the nazgulScreech function (provided) is invoked
+  // add the ring as a child of Frodo
+}
+
+function makeBuddies() {
+  // create an aside tag
+  // attach an unordered list of the 'buddies' in the aside
+  // insert your aside as a child element of rivendell
+}
+
+function beautifulStranger() {
+  // change the 'Strider' textnode to 'Aragorn'
+}
