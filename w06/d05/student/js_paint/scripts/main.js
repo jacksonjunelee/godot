2ca22c46d7724d4dac8f960271a8fe89e7e45f4a
Kamari Aykes <aykeskc@gmail.com>
2015-01-23 16:36:08 -0500

    adds event listeners for tile color change

--- w06/d05/student/js_paint/scripts/main.js
+++ w06/d05/student/js_paint/scripts/main.js
@@ -1,6 +1,16 @@
 var mouseDepressed = false;
 
 var body = document.querySelector("body");
+var canvas = document.getElementsByClassName("canvas");
+var mouseEnter = function(event) {
+    event.target.setAttribute("class", "tile green");
+  };
+for (var i = 0; i < 805; i++) {
+  var tile = document.createElement("div");
+  tile.setAttribute("class", "tile");
+  canvas[0].appendChild(tile);
+  tile.addEventListener("mouseover", mouseEnter);
+}
 
 
 
