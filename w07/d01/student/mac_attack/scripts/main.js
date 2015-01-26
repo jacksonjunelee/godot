22de5849e080fb5cc9543d759cef9d08f33f55a4
Kamari Aykes <aykeskc@gmail.com>
2015-01-26 14:04:40 -0500

    adds morning .js exercise

--- w07/d01/student/mac_attack/scripts/main.js
+++ w07/d01/student/mac_attack/scripts/main.js
@@ -5,7 +5,36 @@ var p  = document.createElement("p");
 var ul = document.createElement("ul");
 p.textContent = "A list of their albums:"
 
+// var addContentToDom = function(){
 // add content to the DOM
+// var elAlbums = document.querySelector(".albums");
+// elAlbums.appendChild(p);
+// elAlbums.appendChild(ul);
+// }
+
+// window.addEventListener("load", addContentToDom);
+//  -------------OR------------------
+window.addEventListener("load", function(){
 var elAlbums = document.querySelector(".albums");
 elAlbums.appendChild(p);
 elAlbums.appendChild(ul);
+
+  for (var i=0, len=fleetwoodMacAlbums.length; i < len; i++){
+    var li = document.createElement("li");
+    li.setAttribute("class", "album");
+    li.textContent = fleetwoodMacAlbums[i].title + ", " + moment(fleetwoodMacAlbums[i].releasedOn).format("MMM Do YYYY");
+    ul.appendChild(li);
+  }
+});
+
+console.log(fleetwoodMacAlbums);
+
+var releaseDate = fleetwoodMacAlbums[0].releasedOn;
+  releaseDate.split(" ")[2];
+
+// RUBY VERSION/////
+// albums.each do { |album| }
+var logTitle = function (album){
+  return album.title;
+}
+
