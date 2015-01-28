9fd652c1d419a7f922f7cf1e47fe67b06e0929e9
Kamari Aykes <aykeskc@gmail.com>
2015-01-28 10:27:01 -0500

    adds jquery updates

--- w07/d03/student/morning_exercise/js/main.js
+++ w07/d03/student/morning_exercise/js/main.js
@@ -6,5 +6,34 @@ var loadJS = function(){
   var head = document.querySelector("head");
   head.appendChild(s);
 };
-
 loadJS();
+
+var loadCSS = function(){
+  var link = document.createElement("link");
+  link.setAttribute("href", "css/style.css");
+  link.setAttribute("rel", "stylesheet");
+  var head = document.head;
+  head.appendChild(link);
+};
+loadCSS();
+
+var loadStyle = function(){
+  var style = document.createElement("style");
+  style.textContent = "body { background: blue; color: white}";
+  document.head.appendChild(style);
+};
+loadStyle();
+
+var colorify = function(){
+  color = window.prompt("Please enter a color:", "");
+  color.setAttribute("innerText", "");
+};
+// colorify();
+
+var jQuerify = function(){
+  var url = "https://code.jquery.com/jquery-1.11.2.min.js";
+  var scriptTag = document.createElement("script");
+  scriptTag.setAttribute("src", url);
+  document.head.appendChild(scriptTag);
+}
+jQuerify();
