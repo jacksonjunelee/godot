891463e0ea10f12153577be565d1342295260028
Kamari Aykes <aykeskc@gmail.com>
2015-01-27 20:50:09 -0500

    adds homework updates tues night

--- w07/d01/student/homework_bond/bondResults.js
+++ w07/d01/student/homework_bond/bondResults.js
@@ -3,8 +3,11 @@ console.log('bondResults.js loaded');
 // Part 1 //
 
 var grossToNumber = function(gross){
-  for (var = 0, len = bondFilms.length; i < len; i++){
-  var gross_string = bondFilms[i]["gross"].replace("$", "").split(",").join("")
-  return parseInt(gross_string);
-  }
+  for (i = 0, len = bondFilms.length; i < len; i++){
+  var gross_string = bondFilms[i]["gross"].replace("$", "").split(",").join("")};
+  return gross_string;
 };
+
+var sumGrosses = function(bondFilms){
+
+}
