7cf801c8841fa585106cb98c14b9c6c91825c9ff
Kamari Aykes <aykeskc@gmail.com>
2015-02-05 17:36:56 -0500

    adds comment outs

--- w08/d04/student/moma_app/seed.rb
+++ w08/d04/student/moma_app/seed.rb
@@ -14,15 +14,15 @@ ActiveRecord::Base.establish_connection(
 
 ActiveRecord::Base.logger = ActiveSupport::Logger.new($stdout) ##OUTPUTS MILLISEC COUNTER##
 
-a1 = Artist.new
-a1.name = "Vincent Van Gogh"
-a1.nationality = "Dutch"
-a1.save
+# a1 = Artist.new
+# a1.name = "Vincent Van Gogh"
+# a1.nationality = "Dutch"
+# a1.save
 
-a2 = Artist.create({name: "Picasso", nationality: "Spanish"})
+# a2 = Artist.create({name: "Picasso", nationality: "Spanish"})
 
-a1.paintings.create({name: "Starry Night"})
-p1 = Painting.new({name: "Sunflowers"})
-a1.paintings << p1
+# a1.paintings.create({name: "Starry Night"})
+# p1 = Painting.new({name: "Sunflowers"})
+# a1.paintings << p1
 
-p2 = Painting.new({name: "The Postman", artist_id: a1.id})
+# p2 = Painting.new({name: "The Postman", artist_id: a1.id})
