79480e32521f94b3319afa242521fb42167efe12
Kamari Aykes <aykeskc@gmail.com>
2015-02-10 18:57:21 -0500

    adds all hw commands

---
+++ w09/d02/student/straight_curdin/app/controllers/cheeses_controller.rb
@@ -0,0 +1,40 @@
+class CheesesController < ApplicationController
+
+  def index
+    @cheeses = Cheese.all
+  end
+
+  def show
+    @cheese = Cheese.find(params[:id])
+  end
+
+  def new
+    @cheese = Cheese.new
+  end
+
+  def create
+    cheese = Cheese.create(cheese_params)
+    cheese.save
+    redirect_to cheeses_path
+  end
+
+  def destroy
+    Cheese.destroy(params[:id])
+    redirect_to cheeses_path
+  end
+
+  def edit
+    @cheese = Cheese.find(params[:id])
+  end
+
+  def update
+    cheese = Cheese.find(params[:id])
+    cheese.update(cheese_params)
+    redirect_to cheeses_path(cheese)
+  end
+
+  def cheese_params
+    params.require(:cheese).permit(:name, :milk_type, :description)
+  end
+
+end
