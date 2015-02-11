364eb388afbdad35c095e8297702fadedbca27c2
Kamari Aykes <aykeskc@gmail.com>
2015-02-11 12:19:11 -0500

    adds movies folders

--- w09/d03/student/popcorn_app/app/controllers/movies_controller.rb
+++ w09/d03/student/popcorn_app/app/controllers/movies_controller.rb
@@ -4,5 +4,36 @@ class MoviesController < ApplicationController
     @movies = Movie.all
   end
 
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    movie = Movie.create(user_params)
+    redirect_to movie_path(movie)
+  end
+
+  def destroy
+    Movie.destroy(params[:id])
+    redirect_to movies_path
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    movie = Movie.find(params[:id])
+    movie.update(movie_params)
+    redirect_to movie_path(movie)
+  end
+
+  def movie_params
+    params.require(:movie).permit(:purchase_price, :rental_price, :available_on, :description, :rating, :poster_image, :length, :title, :created_at, :updated_at)
+  end
 
 end
--- w09/d03/student/popcorn_app/config/routes.rb
+++ w09/d03/student/popcorn_app/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'users#index'
   get '/test' => 'users#test'
-  resources :users
+  resources :users, :movies
 
-  root 'movies#index'
-  resources :movies
+  # root 'movies#index'
+  # resources
 
 end
