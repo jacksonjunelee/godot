f6209770e9099e66f46ad53c24081befc151640c
Kamari Aykes <aykeskc@gmail.com>
2015-02-13 10:15:52 -0500

    updates pig latin rspec

--- w09/d05/student/pig_latin/lib/word.rb
+++ w09/d05/student/pig_latin/lib/word.rb
@@ -3,13 +3,13 @@ class Word
     @original_word = original_word
   end
 
-  def piglatinize(word)
-    word = @original_word
-    if word.start_with?('a', 'e', 'i', 'o', 'u')
-      word << "way"
-    #   binding.pry
-    # elsif original_word.start_with
+  def piglatinize
+    if @original_word.start_with?('a', 'e', 'i', 'o', 'u')
+      @original_word << "way"
+    elsif
+      @original_word.split('').rotate.join('') + 'ay'
 
+      original_word.split('').rotate(2).join('') + 'ay'
 
     end
   end
