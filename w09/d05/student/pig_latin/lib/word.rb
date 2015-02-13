81ad1de06da22af1b565087800a3b6015cc6dfa2
Kamari Aykes <aykeskc@gmail.com>
2015-02-13 10:12:03 -0500

    adds pig latin rspec

--- w09/d05/student/pig_latin/lib/word.rb
+++ w09/d05/student/pig_latin/lib/word.rb
@@ -2,4 +2,15 @@ class Word
   def initialize(original_word)
     @original_word = original_word
   end
+
+  def piglatinize(word)
+    word = @original_word
+    if word.start_with?('a', 'e', 'i', 'o', 'u')
+      word << "way"
+    #   binding.pry
+    # elsif original_word.start_with
+
+
+    end
+  end
 end
