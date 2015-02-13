6af9ebba68f7daa291ff789368249c133168adaa
Kamari Aykes <aykeskc@gmail.com>
2015-02-13 16:36:20 -0500

    adds seeds.rb details

--- w09/d05/student/bookish_app/db/seeds.rb
+++ w09/d05/student/bookish_app/db/seeds.rb
@@ -7,3 +7,17 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Author.destroy_all
+Book.destroy_all
+Character.destroy_all
+
+lee = Author.create(name:"Harper Lee", born_on: 19260428, died_on:00000000, gender:"female", has_pseudonym: "")
+twain = Author.create(name:"Mark Twain", born_on: 18351120, died_on: 19100421, gender:"male", has_pseudonym: "true")
+king = Author.create(name:"Stephen King", born_on: 19470921, died_on: 00000000, gender:"male", has_pseudonym: "")
+
+mockingbird = Book.create(title:"To Kill A Mockingbird", released_on: 19600711, genre:"Southern Gothic")
+finn = Book.create(title:"Adventures of Huckleberry Finn", released_on:18850201 , genre:"Satirical Novel")
+shining = Book.create(title:"The Shining", released_on:, genre:"Horror")
+
+boo = Character.create(name:"Boo Radley", gender:"male", quirk:"super shy")
+jim = Character.create(name:"Jim", gender:"male", quirk:"escaped former slave")
+danny = Character.create(name:"Danny Torrance", gender:"male", quirk:"telepathic")
