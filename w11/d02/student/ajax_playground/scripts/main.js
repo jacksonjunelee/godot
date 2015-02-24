59b6bb7ddce6442ae66542d3b6cc245ebb69400e
Kamari Aykes <aykeskc@gmail.com>
2015-02-24 13:34:46 -0500

    modified api random

--- w11/d02/student/ajax_playground/scripts/main.js
+++ w11/d02/student/ajax_playground/scripts/main.js
@@ -4,29 +4,69 @@ $(document).ready(function(){
   // caches reference to body element
   var $body = $("body");
 
+  $("#random-user-btn").on('click', function() {
+    $.ajax({
+      url: "http://api.randomuser.me",
+      type: "GET",
+      dataType: "json"
+    }).done(function(data){
+      var user = data["results"][0]["user"];
+      var pictureUrl = user["picture"]["medium"];
+      var image = $("<img>").attr("src", pictureUrl);
+      $("body").append(image);
+    });
+  });
+
+  $("#female-user-btn").on('click', function() {
+      $.ajax({
+        url: "http://api.randomuser.me",
+        type: "GET",
+        data: { gender: "female" },
+        dataType: "json"
+      }).done(function(data){
+        var user = data["results"][0]["user"];
+        var pictureUrl = user["picture"]["medium"];
+        var image = $("<img>").attr("src", pictureUrl);
+        $("body").append(image);
+      });
+    });
+
+    $("#male-user-btn").on('click', function() {
+      $.ajax({
+        url: "http://api.randomuser.me",
+        type: "GET",
+        data: { gender: "male" },
+        dataType: "json"
+      }).done(function(data){
+        var user = data["results"][0]["user"];
+        var pictureUrl = user["picture"]["medium"];
+        var image = $("<img>").attr("src", pictureUrl);
+        $("body").append(image);
+      });
+    });
+
   $.ajax({
     url: "http://api.randomuser.me",
     type: "GET",
     dataType: "json"
   }).done(function(data){
-    var user = data["results"][0]["user"];
-    var pictureUrl = user["picture"]["medium"];
-    var image = $("<img>").attr("src", pictureUrl);
-    $("body").append(image);
-  });
-
+      var user = data["results"][0]["user"];
+      var pictureUrl = user["picture"]["medium"];
+      var image = $("<img>").attr("src", pictureUrl);
+      $("body").append(image);
+    });
 });
 
 // on line 7, what is "$"
   // $ calls the DOM in jQuery
 // on line 7, what is .ajax
-  // calls the ajax function
+  // calls the ajax method for jQuery
 // on line 7, what is {
-  // {
+  // { opens json object
 // on line 8, what is url ?
-  // url is the location for the GET request
+  // url is the location for the GET request (key)
 // on line 8, what is "api.randomuser.me" ?
-  // source of the json
+  // value; source of the json
 // on line 8, what is type ?
   // type of ajax request
 // on line 8, what is GET ?
@@ -34,8 +74,8 @@ $(document).ready(function(){
 // on line 10, what is "json" ?
   // data format
 // on line 11, what is .done ?
-  // method to access the json hash
+  // method to access the ajax json hash
 // on line 11, what is function ?
   // callback fxn to access the json data
 // on line 11, what is data ?
-  // info received in the results hash
+  // info received in the results hash (name param)
