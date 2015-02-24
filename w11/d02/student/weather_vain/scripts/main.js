fced7b093a37bee8cd24b0384848b30d2dada8a1
Kamari Aykes <aykeskc@gmail.com>
2015-02-24 15:04:04 -0500

    does weather api edits

--- w11/d02/student/weather_vain/scripts/main.js
+++ w11/d02/student/weather_vain/scripts/main.js
@@ -1,9 +1,36 @@
 $(document).ready(function(){
+  // $(e).preventDefault();
   console.log("document ready");
 
+  var $body = $("body");
   // sets the day's date at the bottom of viewport
   var $date = $(".date");
   var currentDateString = moment().format('dddd, MMMM Do');
   $date.text(currentDateString);
+
+
+  var button = $(".pure-button");
+
+  button.on('click', function(e) {
+    e.preventDefault();
+    var desiredCity = $(".pure-input-rounded").val();
+  $.ajax({
+    url: "http://api.openweathermap.org/data/2.5/weather",
+    data: { q: desiredCity },
+    type: "GET",
+    dataType: "json"
+    }).done(function(data){
+      // debugger
+      var cityName = data.name;
+      var temp = data.main.temp;
+
+      $("#forecast").text("The temperature in " + cityName + " is: " + temp + " degrees Kelvin.");
+
+    });
+  });
+
+
+  // current weather for Miwaukee, WI
+
 
 });
