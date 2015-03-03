1f03048da688e36e61aff9025cbbd391842f0bc6
Kamari Aykes <aykeskc@gmail.com>
2015-03-03 16:20:00 -0500

    adds afternoon backbone.js updates

--- w12/d02/student/tmnt/app/assets/javascripts/main.js
+++ w12/d02/student/tmnt/app/assets/javascripts/main.js
@@ -8,20 +8,34 @@ url: "http://localhost:3000/reporter"
 // Here, we instantiate a new ReporterModel
 var april = new ReporterModel();
 
-april.get('name') // => undefined
+april.get('name'); // => undefined
 
 // makes GET request to specified URL & sets mirrored attributes
 april.fetch();
 
-april.get('name') //
+april.get('name'); //
 
 var RatModel = Backbone.Model.extend({
-  url: 'http://localhost:3000/rat'
+  url: 'rat'
+});
+
+var RatView = Backbone.View.extend({
+  el: '.profile',
+  initialize: function(){
+    this.listenTo(this.model, 'change', this.render);
+    this.render();
+  },
+
+  render: function(){
+    this.$el.html('<h1>' + this.model.get('name') + '</h1>');
+  }
 });
 
 var splinter = new RatModel();
 
-splinter.fetch();
+splinter.fetch().done(function(){
+  ratView = new RatView({ model: splinter });
+});
 // debugger
 
 var FootSoldier = Backbone.Model.extend({
